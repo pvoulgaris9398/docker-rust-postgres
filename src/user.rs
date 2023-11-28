19c819ffbd43573585ccbe59e6c190ed447060b8
Peter Voulgaris <40342772+pvoulgaris9398@users.noreply.github.com>
2023-11-28 10:52:01 -0500

    Update user.rs

--- src/user.rs
+++ src/user.rs
@@ -4,6 +4,7 @@ use tokio_postgres::{Error, GenericClient, Row};
 pub struct User {
     pub id: i32,
     pub login: String,
+    pub name: String,
 }
 
 impl From<Row> for User {
@@ -11,13 +12,14 @@ impl From<Row> for User {
         Self {
             id: row.get(0),
             login: row.get(1),
+            name: row.get(2),
         }
     }
 }
 
 impl User {
     pub async fn all<C: GenericClient>(client: &C) -> Result<Vec<User>, Error> {
-        let stmt = client.prepare("SELECT id, login FROM users").await?;
+        let stmt = client.prepare("SELECT id, login, 'name' = 'Napoleon' FROM users").await?;
         let rows = client.query(&stmt, &[]).await?;
 
         Ok(rows.into_iter().map(User::from).collect())
